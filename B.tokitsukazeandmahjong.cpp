0b3f70a447aee0555b483cd8b9691e8fdceeefe5
jesuswr <jesuswahrman@gmail.com>
2019-07-12 18:29:26 -0400

    solved B

--- B.tokitsukazeandmahjong.cpp
+++ B.tokitsukazeandmahjong.cpp
@@ -18,10 +18,13 @@ int main()
 	scanf("%d%c",&a,&c);
 	cards[2]={a,c};
 	sort(cards,cards+3);
+	
 	if (cards[0].first == cards[1].first && cards[0].second == cards[1].second && cards[1].first == cards[2].first && cards[2].second == cards[1].second) {printf("0\n");return 0;}
 	if (cards[0].second==cards[1].second && cards[1].second==cards[2].second && cards[0].first==cards[1].first-1 && cards[1].first == cards[2].first-1) {printf("0\n");return 0;}
 	if (cards[1].first==cards[2].first && cards[1].second==cards[2].second) {printf("1\n");return 0;}
 	if (cards[1].first==cards[0].first && cards[1].second==cards[0].second) {printf("1\n");return 0;}
+	if (cards[0].first== cards[2].first-2 && cards[0].second == cards[2].second) {printf("1\n");return 0;}
+	if (cards[0].first== cards[2].first-1 && cards[0].second == cards[2].second) {printf("1\n");return 0;}
 	if ( (cards[1].first==cards[2].first-1 || cards[1].first==cards[2].first-2) && cards[1].second == cards[2].second ) {printf("1\n");return 0;}
 	if ( (cards[0].first==cards[1].first-1 || cards[0].first==cards[1].first-2) && cards[0].second == cards[1].second ) {printf("1\n");return 0;}
 	printf("2\n");
