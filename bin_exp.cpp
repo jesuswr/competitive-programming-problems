9f198d517bf20490baef505bbfe0636822167bd3
jesuswr <jesuswahrman@gmail.com>
2019-10-25 20:25:29 -0400

    bin exp

---
+++ bin_exp.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <cstring>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<int,pair<int,int>> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+#define ri(a) scanf("%d", &a)
+#define rii(a,b) scanf("%d %d", &a, &b)
+#define riii(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define rl(a) scanf("%lld", &a)
+#define rll(a,b) scanf("%lld %lld", &a, &b)
+#define FOR(i,n,m) for(int i=n; i<m; i++)
+#define ROF(i,n,m) for(int i=n; i>m; i--)
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+
+const int INF = 0x3f3f3f3f;
+const ll LLINF = 1e18;
+const int maxN = 1e5; // CAMBIAR ESTE
+
+// GJNM
+unsigned long long pow( unsigned long long b , unsigned long long e )
+{
+	if ( e == 1 ) return b;
+	if ( e == 0 ) return 1;
+	unsigned long long ret;
+	ret = pow(b,e/2);
+	ret *= ret;
+	if ( e&1 ) ret *= b;
+	return ret;
+}
+
+long long binpow(long long a, long long b) {
+    if (b == 0)
+        return 1;
+    long long res = binpow(a, b / 2);
+    if (b % 2)
+        return res * res * a;
+    else
+        return res * res;
+}
+
+
+int main()
+{
+	unsigned long long a, b;
+	rll(a,b);
+	unsigned long long ans = pow(a,b);
+	ll ans2 = binpow( a , b);
+	printf("%llu\n", ans);
+	printf("%lld\n", ans2);
+	return 0;
+}
