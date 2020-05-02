2840c91f59961db963b608483e3a59588908640a
jesuswr <jesuswahrman@gmail.com>
2020-05-02 16:50:01 -0400

    dp and change to template

--- 10337.flight_planner.cpp
+++ 10337.flight_planner.cpp
@@ -36,8 +36,50 @@ const ll LLINF = 1e18;
 const int maxN = 1e5; // CAMBIAR ESTE
 
 // GJNM
+int x;
+int A[10][1001], dp[10][1001];
+
+int f(int h, int d){
+	if ( d == x && h == 0 )
+		return 0;
+	if ( h<0 || h>9)
+		return INF;
+	if ( d == x )
+		return INF;
+	if ( dp[h][d] != -1 )
+		return dp[h][d];
+
+	int &ret = dp[h][d];
+	ret = -A[h][d];
+
+	ret += min(
+				f(h+1,d+1) + 60, // up
+				min(
+					f(h,d+1) + 30, // same
+					f(h-1,d+1) +20 // down
+				   )
+			  );
+	return ret;
+
+}
 
 int main(){
+	int t;
+	ri(t);
+	while(t--){
+		ri(x);
+		x /= 100;
 
+		ROF(i,9,-1)
+			FOR(j,0,x){
+				ri(A[i][j]);
+				dp[i][j] = -1;
+			}
+
+
+		
+		printf("%d\n\n", f(0,0));
+		
+	}
 	return 0;
 }
