7796816b809d3bd0429e7873f203245db002a6dd
jesuswr <jesuswahrman@gmail.com>
2020-06-02 15:32:46 -0400

    diophantine equations

---
+++ A.ebony_and_ivory.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <cstring>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<int,pair<int,int>> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+#define ri(a) scanf("%d", &a)
+#define rii(a,b) scanf("%d %d", &a, &b)
+#define riii(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define rl(a) scanf("%lld", &a)
+#define rll(a,b) scanf("%lld %lld", &a, &b)
+#define FOR(i,n,m) for(int i=n; i<m; i++)
+#define ROF(i,n,m) for(int i=n; i>m; i--)
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define F first
+#define S second
+
+const int INF = 0x3f3f3f3f;
+const ll LLINF = 1e18;
+const int maxN = 1e5; // CAMBIAR ESTE
+
+// GJNM
+int gcd(int a, int b, int& x, int& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+bool find_any_solution(int a, int b, int c, int &x0, int &y0, int &g) {
+    g = gcd(abs(a), abs(b), x0, y0);
+    if (c % g) {
+        return false;
+    }
+
+    x0 *= c / g;
+    y0 *= c / g;
+    if (a < 0) x0 = -x0;
+    if (b < 0) y0 = -y0;
+    return true;
+}
+
+
+int main(){
+	int a,b,c;
+	riii(a,b,c);
+	int g = __gcd(a,b);
+	if ( c%g != 0 ){
+		printf("No\n");
+		return 0;
+	}
+	int x,y;
+	a /= g;
+	b /= g;
+	c /= g;
+	find_any_solution(a,b,c,x,y,g);
+
+	if( x < 0 ){
+		int add = ( ( -x + (b -1) ) / (b) );
+		x += add*b;
+		y -= add*a;
+	}
+
+	if( y < 0 ){
+		int add = ( ( -y + (a -1) ) / (a) );
+		x -= add*b;
+		y += add*a;
+	}
+
+	if ( x < 0 || y < 0 )
+		printf("No\n");
+	else
+		printf("Yes\n");
+
+	return 0;
+}
