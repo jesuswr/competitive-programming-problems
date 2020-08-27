906f5ea85f67f70e6451c11848dbb0a72ae7fa22
jesuswr <jesuswahrman@gmail.com>
2020-08-27 15:54:24 -0400

    st

---
+++ C.number_of_inversions_on_segment.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <cstring>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<int, pair<int, int>> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+#define ri(a) scanf("%d", &a)
+#define rii(a,b) scanf("%d %d", &a, &b)
+#define riii(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define rl(a) scanf("%lld", &a)
+#define rll(a,b) scanf("%lld %lld", &a, &b)
+#define FOR(i,n,m) for(int i=n; i<m; i++)
+#define ROF(i,n,m) for(int i=n; i>m; i--)
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define F first
+#define S second
+
+const int INF = 0x3f3f3f3f;
+const ll LLINF = 1e18;
+const int MAXN = 1e5; // CAMBIAR ESTE
+
+// GJNM
+// Todos los rangos son semi-abiertos [a,b)
+int N, A[MAXN];
+struct STN {
+	ll cnt[41] = {0};
+	ll val = 0;
+	ll inv;
+	void merge(STN& L, STN& R) {
+		inv = L.inv + R.inv;
+		FOR(i, 1, 41) {
+			FOR(j, i + 1, 41) {
+				if ( R.cnt[i] == 0 )
+					break;
+				inv += (L.cnt[j] * R.cnt[i]);
+			}
+			cnt[i] = L.cnt[i] + R.cnt[i];
+		}
+	}
+	void operator=(ll a) {
+		cnt[val] = 0;
+		cnt[val = a] = 1;
+		inv = 0;
+	}
+};
+STN ST[4 * MAXN];
+void STB(int id = 1, int l = 0, int r = N) {
+	if (r - l < 2) {
+		ST[id] = A[l];
+		return;
+	}
+	int m = (l + r) >> 1, L = id << 1, R = L | 1;
+	STB(L, l, m); STB(R, m, r);
+	ST[id].merge(ST[L], ST[R]);
+}
+STN STQ(int x, int y, int id = 1, int l = 0, int r = N) {
+	if (x == l && y == r) return ST[id];
+	int m = (l + r) >> 1, L = id << 1, R = L | 1;
+	if (x >= m) return STQ(x, y, R, m, r);
+	if (y <= m) return STQ(x, y, L, l, m);
+	STN res, ln = STQ(x, m, L, l, m), rn = STQ(m, y, R, m, r);
+	return res.merge(ln, rn), res;
+}
+void STU(int p, int x, int id = 1, int l = 0, int r = N) {
+	if (r - l < 2) {
+		ST[id] = x;
+		return;
+	}
+	int m = (l + r) >> 1, L = id << 1, R = L | 1;
+	if (p < m) STU(p, x, L, l, m);
+	else STU(p, x, R, m, r);
+	ST[id].merge(ST[L], ST[R]);
+}
+
+int main() {
+	int q;
+	rii(N, q);
+	FOR(i, 0, N) {
+		ri(A[i]);
+	}
+	STB();
+	FOR(i, 0, q) {
+		int type;
+		ri(type);
+		if (type == 1) {
+			int l, r;
+			rii(l, r); l--;
+			printf("%lld\n", STQ(l, r).inv);
+		}
+		else {
+			int x, y;
+			rii(x, y); x--;
+			STU(x, y);
+		}
+	}
+
+	return 0;
+}
